13e69b120fbbada9e5e704879e687357729dd4c5
Roman-Braneshty <102828727+Roman-Braneshty@users.noreply.github.com>
2022-06-15 20:32:53 +0300

    Update adressbook.py

--- utils/adressbook.py
+++ utils/adressbook.py
@@ -1,3 +1,7 @@
 class AdressBook:
-    def __init__(self, name):
-        self.name = name
+    def __init__(self, fullname, phone):
+        self.fullname = fullname
+        self.phone = phone
+
+    def add_new_phone(self, phone):
+        self.phone = phone
